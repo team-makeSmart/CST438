48a57d1e4181a5316d8c0099c7d79d0db5fb8329
EC2 Default User <ec2-user@ip-172-31-20-110.us-west-2.compute.internal>
2019-03-26 21:36:01 +0000

    a5

---
+++ app/models/expense.rb
@@ -0,0 +1,4 @@
+class Expense < ApplicationRecord
+	  belongs_to :user
+	  validates :content, length: { maximum: 140 },presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	  has_many :expenses
+	  validates :name, presence: true    # Replace FILL_IN with the right code.
+    validates :email, presence: true    # Replace FILL_IN with the right code.
+
 end
