55b6def568767814c6541c46a923723a00a86bca
EC2 Default User <ec2-user@ip-172-31-20-110.us-west-2.compute.internal>
2019-03-26 22:00:10 +0000

    a6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
 	  has_many :expenses
-	  validates :name, presence: true    # Replace FILL_IN with the right code.
-    validates :email, presence: true    # Replace FILL_IN with the right code.
+	  
+	  before_save { self.email = email.downcase }
+	  validates :name,  presence: true, length: { maximum: 50 }
+	  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190326212542) do
+ActiveRecord::Schema.define(version: 20190326214735) do
 
   create_table "expenses", force: :cascade do |t|
     t.text     "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190326212542) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
