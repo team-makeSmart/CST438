9d990c02051d49f1026db1b22f96bad6337b0e62
pavlosp <pavlosp1967@gmail.com>
2019-03-26 22:23:43 -0700

    update sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
 
   resources :expenses
   resources :users
-  
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
