1a2ada180896871603735aadd86509755ddb8215
pavlosp <pavlosp1967@gmail.com>
2019-04-15 14:19:57 -0700

    update date format for queries

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,15 +3,20 @@ class DashboardController < ApplicationController
 
   def index
 
+    unless params['from'].nil? || params['to'].nil?
+
     @from = params['from']
     @to = params['to']
+
     @last_expense = Expense.first.created_at.strftime("%Y-%m-%d")
 
-    records = Expense.select(:amount, :created_at).where("created_at >= ? AND created_at <= ? ","#{@from} 00:00:01","#{@to} 23:59:59")
+    # records = Expense.select(:amount, :created_at).where("created_at >= ? AND created_at <= ? ","#{@from} 00:00:01","#{@to} 23:59:59")
+    records = Expense.select(:amount, :created_at).where(created_at: (Date.parse(@from) -1.day)...Date.parse(@to)+ 1.day)
+
     @empty_records = records == []
     amount, date = data_to_json(records)
     chart(amount, date)
-
+    end
   end
 
 
