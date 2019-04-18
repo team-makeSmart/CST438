32c3b592e6a527232ec3b1ea9ad246c27f45400f
pavlosp <pavlosp1967@gmail.com>
2019-04-17 18:29:08 -0700

    added sign up

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,5 +1,8 @@
-$('.modal-body input').datepicker({
-    format: 'yyyy-mm-dd'
-}).on('change', function () {
-    $('.datepicker').hide();
-});
+// $(document).on('turbolinks:load', function () {
+//     $('.modal-body input').datepicker({
+//         format: 'yyyy-mm-dd'
+//     }).on('change', function () {
+//         $('.datepicker').hide();
+//     });
+// });
+
