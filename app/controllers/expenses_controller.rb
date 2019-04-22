4de88f3721b90b9c94c298820b5aa22957a528d5
Jake McGhee <33072387+Mcghee-Jake@users.noreply.github.com>
2019-04-21 22:57:57 -0700

    Update expenses_controller.rb

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -86,6 +86,11 @@ class ExpensesController < ApplicationController
   # For creating an expense from the android app
   def post_json_expense
     Expense.new(expense_params)
+    if expense.save
+      render :json  
+      {
+        status: 200
+      }.to_json
   end
   
   # Never trust parameters from the scary internet, only allow the white list through.
