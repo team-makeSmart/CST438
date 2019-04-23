e49a8e847d613732af5858e3779019163e745bce
pavlosp <pavlosp1967@gmail.com>
2019-04-23 07:34:51 -0700

    update chart link

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w( datepicker.js )
+Rails.application.config.assets.precompile += %w( graph.js )
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
