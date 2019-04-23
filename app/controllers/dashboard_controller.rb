90aa703cb5033af6c71432e8f775ae49cbf7504c
pavlosp <pavlosp1967@gmail.com>
2019-04-23 09:01:38 -0700

    fixed messages on select by date

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,33 +10,12 @@ class DashboardController < ApplicationController
   private
 
   def get_records
-    @valid = true
-
-    date_input
-
-    if @valid
-      @from = params['from']
-      @to = params['to']
-      @last_expense = Expense.first.created_at.strftime("%Y-%m-%d")
-      # records = Expense.select(:amount, :created_at).where("created_at >= ? AND created_at <= ? ","#{@from} 00:00:01","#{@to} 23:59:59")
-      @expenses = Expense.where("created_at >= ? AND created_at <= ?", (Date.parse(@from) - 1.day), Date.parse(@to) + 36.hour)
-
-      @empty_records = @expenses == []
-
-    end
+    @from = params['from']
+    @to = params['to']
+    @last_expense = Expense.first.created_at.strftime("%Y-%m-%d")
+    @expenses = Expense.find_by_sql("select * from expenses where user_id = #{current_user.id} and created_at >= #{(Date.parse(@from) - 1.day)} and created_at <= '#{(Date.parse(@to) + 36.hour)}' ")
+    @empty_records = @expenses == []
   end
-
-  def date_input
-    begin
-      Date.parse(params['from'])
-      Date.parse(params['to'])
-    rescue ArgumentError
-      @valid = false
-    end
-  end
-
-
-
 
 end
 
